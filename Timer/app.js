cfd596b5e808890ef391a2b7124aae29e448f654
Maroof Ahmad <Maroof Ahmad>
2016-07-05 02:26:39 +0530

    timer added

---
+++ Timer/app.js
@@ -0,0 +1,33 @@
+var Timer = React.createClass({
+
+	getInitialState : function(){
+		return {
+			elapsed : 0
+		};
+	},
+
+	componentDidMount : function(){
+		this.timer = setInterval(this.tick , 50);
+	},
+
+	componentWillUnmount : function(){
+		clearInterval(this.timer);
+	},
+
+	tick : function(){
+		this.setState({elapsed: new Date() - this.props.start});
+	},
+
+	render : function(){
+		var elapsed = Math.round(this.state.elapsed / 100);
+		var seconds = (elapsed/10).toFixed(1);
+
+		return <p>Timer started <b>{seconds} seconds</b> ago </p>;
+	}
+});
+
+
+ReactDOM.render(
+	<Timer start={Date.now()} />,
+	document.getElementById('mydiv')	
+);
